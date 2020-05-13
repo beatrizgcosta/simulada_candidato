ea68aa7cae1533dfa11b8da8bd543eaf973e38a4
Beatriz Gomes <bia.beatrizgomes93@gmail.com>
2020-05-13 19:41:05 -0300

    Leitura da escolaridade

---
+++ src/programa.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int escolaridade;
+		
+		// LEITURA DA ESCOLARIDADE
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.printf("Digite uma opção: ");
+		escolaridade = sc.nextInt();
+		
+		sc.close();
+	}
+
+}
